c617a962f5ccdad9969a9847df183a4a8ff81947
JSD <tjden0305@gmail.com>
2021-12-29 16:43:55 +0900

    Set: Setting start tyepscript on react

---
+++ practice1/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Hello, world</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
