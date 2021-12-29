e9fb1d62d35da4f987302dd5fd93179034cd2b70
JSD <tjden0305@gmail.com>
2021-12-29 21:07:49 +0900

    client: Add Login Modal

--- practice1/src/App.tsx
+++ practice1/src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
+import LoginModal from './Component/LoginModal';
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
+
+  const clickLogin = () => {
+    setIsModalVisible(true);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>Hello, world</div>
+        <button type="button" onClick={clickLogin}>login</button>
+        <div>
+          <LoginModal isModalVisible={isModalVisible} setIsModalVisible={setIsModalVisible} setIsLogin={setIsLogin} />
+        </div>
+        <div>
+          {isLogin ? 
+          <div>로그인 성공</div> : null}
+        </div>
       </header>
     </div>
   );
